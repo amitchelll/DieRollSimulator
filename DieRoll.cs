f533e295761095609888baff047125492c492976
allie <allie@ALEXANDRAMI8C4F>
2024-01-18 21:45:59 -0700

    Add project files.

---
+++ DieRoll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRollSimulator
+{
+    internal class DieRoll
+    {
+        // When a new instance of DieRoll is created the variable random is also created
+        private Random random;
+        public DieRoll() 
+        { 
+            random = new Random();
+        }
+        //The method RollSimulator is passed the dice roll number and returns an array of integers
+        public int[] RollSimulator(int diceRollNum)
+        {
+            //The results can only be 2-12 so the size of the array is 13
+            int[] rollResults = new int[13];
+
+            //As long as the counter is less than the number of desired rolls the loop with generate a random number (1-6) for each die
+            //The die are added together and added to the array
+            for (int i = 0; i < diceRollNum; i++)
+            {
+                int dice1Num = random.Next(1, 7);
+                int dice2Num = random.Next(1, 7);
+                int dieSum = dice1Num + dice2Num;
+
+                rollResults[dieSum]++;
+            }
+            return rollResults;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using DieRollSimulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Welcome Message that prompts user to type in the number of time they want to roll the dice.
+        Console.WriteLine("Welcome to the Dice Simulator!\nHow many dice rolls would you like to simulate?\n");
+
+        // Sets an integer variable equal to the number entered into the command prompt after parsing the data type to an integer.
+        int diceRollNum = int.Parse(Console.ReadLine());
+
+        // This uses the DieRoll class to create a roll object. The RollSimulator method is accessed and the number of rolls is passed to it.
+        DieRoll roll = new DieRoll();
+        int[] rollResults = roll.RollSimulator(diceRollNum);
+
+        // Explains what the histogram is showing
+        Console.WriteLine("Dice Roll Results:");
+        Console.WriteLine($"Each * represents 1% of the total number of rolls.\nThe total number of rolls is {diceRollNum}.\n");
+
+        //Iterates through the rollResults array and prints out the roll number and correct number of * symbols based on the calculated percentage.
+        for (int i = 2; i <= 12; i++)
+        {
+            int rollPercentage = rollResults[i] * 100 / diceRollNum;
+            Console.WriteLine($"{i}: {new string('*', rollPercentage)}");
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator.\nHave a great day and play again soon!");
+
+
+
+    }
+}
